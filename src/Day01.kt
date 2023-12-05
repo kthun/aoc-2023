16aa6a8fed7af2984784a1d2d77e6a2a8a424c22
Kristian Thun <kristian.thun@sintef.no>
2023-12-05 11:32:40 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,39 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val first = line.first { it.isDigit() }
+            val last = line.last { it.isDigit() }
+            "$first$last".toInt()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val digits = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        return input.sumOf { line ->
+            val firstString = line.findAnyOf(digits) ?: Pair(Int.MAX_VALUE, "0")
+            val lastString = line.findLastAnyOf(digits) ?: Pair(Int.MIN_VALUE, "0")
+
+            val firstDigit = line.withIndex().firstOrNull { it.value.isDigit() } ?: IndexedValue(Int.MAX_VALUE, '0')
+            val lastDigit = line.withIndex().lastOrNull { it.value.isDigit() } ?: IndexedValue(Int.MIN_VALUE, '0')
+
+            val firstValue = if (firstString.first < firstDigit.index) {
+                digits.indexOf(firstString.second)
+            } else firstDigit.value.digitToInt()
+
+            val lastValue = if (lastString.first > lastDigit.index) {
+                digits.indexOf(lastString.second)
+            } else lastDigit.value.digitToInt()
+
+            "$firstValue$lastValue".toInt()
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput1 = readInput("Day01_test1")
+    check(part1(testInput1) == 142)
+    val testInput2 = readInput("Day01_test2")
+    check(part2(testInput2) == 281)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01_1")
     part1(input).println()
     part2(input).println()
 }
